64622f835f248a076ff88348f239b12e6f28ab58
HuiPan-Peter <132380239+HuiPan-Peter@users.noreply.github.com>
2023-11-02 20:39:01 -0300

    update

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // sequelize object with .env config parameter used throughout app
-// create connection to our tech_blog_db
+// create connection to our tech_blog_db;
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
