1f3157ff03e464f1ffe797092ece15ce521b92c3
HuiPan-Peter <132380239+HuiPan-Peter@users.noreply.github.com>
2023-11-04 16:17:21 -0300

    Update user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -48,7 +48,6 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
